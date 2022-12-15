1bfcd82534af0cb3ecf01c4cf02e178f95b10880
furkanGit <furkancaliskan9801@gmail.com>
2022-12-15 13:58:17 +0100

    Create AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script >
+    export default{
+        name: "AppHeader"
+    }
+</script>
+
+<template>
+ <header>
+    <h1>Rick and Morty Api</h1>
+ </header>
+</template>
+
+<style lang="scss" scoped>
+header{
+    display: flex;
+    padding: 10px;
+}   
+    h1{
+    color: rgb(46, 43, 226);
+    margin: 0 auto;
+
+    }
+</style>
