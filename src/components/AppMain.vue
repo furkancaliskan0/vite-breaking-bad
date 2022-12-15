5ff2076db589dbe921a76eb2dcd40710d113ed72
furkanGit <furkancaliskan9801@gmail.com>
2022-12-15 13:59:59 +0100

    Create Component AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,43 @@
+<script >
+import Card from "./Card.vue"
+import {store} from '../store.js'
+   export default{
+    
+        name:"AppMain",
+        components:{
+            Card,
+        },
+        data() {
+            return {
+                store,
+            }
+        },
+   }
+</script>
+
+<template>
+  <main>
+        <section id="card-container">
+            <div class="card" v-for="character in store.characterList" 
+            :key="character.id">
+            <Card :info="character"/>
+            </div>
+      
+        </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+    #card-container{
+        margin: auto;
+        width:100%;
+        background: yellow;
+        display: flex;
+        justify-content:center;
+        flex-wrap: wrap;
+        .card{
+            text-align: center;
+            margin: 10px;
+        }
+    }
+</style>
