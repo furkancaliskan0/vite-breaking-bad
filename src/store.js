91a211a2f9957826a5f15ab94b2ec6ee084d5b5c
furkanGit <furkancaliskan9801@gmail.com>
2022-12-15 23:23:08 +0100

    store upgrade

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
-import{reactive} from 'vue'
-export const store = reactive(
-    { characterList:[],
-        apiUrl:"https://rickandmortyapi.com/api/character"
-        
+import{reactive} from 'vue';
 
-    });
+export const store = reactive({
+    characterList: [],
+    apiUrl: "https://rickandmortyapi.com/api/character",
+    searchStatus: "choice", 
+    apiStatus: "status"
+});
