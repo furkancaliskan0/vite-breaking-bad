ef244d6b44542c9e4019d1ecf8c103000a2c631f
furkanGit <furkancaliskan9801@gmail.com>
2022-12-15 14:00:20 +0100

    Create Component Card

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<script >
+   export default{
+        name:"Card",
+        props:["info"],
+       
+   }
+</script>
+
+<template>
+    <section id="card">
+            <img :src="info.image" alt="">
+                <h4>{{info.name}}</h4>
+                <h4>{{info.status}}</h4>
+                <h4>{{info.species}}</h4>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+        h4{
+            margin: 15px 0;
+            text-align: center;
+        }
+
+        img {
+            border-radius: 50%;
+        }
+
+</style>
