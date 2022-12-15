3529363d2f0ffc7f2d6e07bfb8491f0df19138f2
furkanGit <furkancaliskan9801@gmail.com>
2022-12-15 14:07:31 +0100

    page completed

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,48 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import { store } from './store.js';
+export default{
+  data() {
+    return {
+      store,
+    }
+  },
+  methods:{
+    getCharacter(){
+      axios
+      .get(store.apiUrl)
+      .then(res =>{
+        store.characterList = res.data.results;
+      })
+      .catch(err =>{
+        console.log("Errori",err);
+      }
+      )
+     
+    }
+   
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    
+    
+  },
+  mounted() {
+        this.getCharacter();
+      }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
+  <AppHeader />
+  <AppMain/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss">
+
 </style>
