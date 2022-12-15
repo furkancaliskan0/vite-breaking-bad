e2b5342ae6d0d2a17d45f9a137512acdc578aa00
furkanGit <furkancaliskan9801@gmail.com>
2022-12-15 23:25:27 +0100

    AppHeader +variables upgrade

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,13 +11,18 @@
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as*;
+
 header{
+    height: 100px;
     display: flex;
     padding: 10px;
 }   
     h1{
-    color: rgb(46, 43, 226);
+    color:$main-color;
     margin: 0 auto;
+    text-align: center;
+    font-size: 80px;
 
     }
 </style>
